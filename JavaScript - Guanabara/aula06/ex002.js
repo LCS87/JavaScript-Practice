119fdc9eb2092baafca3ce91a0702eced895ae55
Leandro Costa <lcostasantos1987@gmail.com>
2022-01-02 19:10:30 -0300

    separação do scrip
    separando javascript do html

---
+++ JavaScript - Guanabara/aula06/ex002.js
@@ -0,0 +1,2 @@
+var nome = window.prompt('Qual o seu nome?'); // pede dados dos usuarios e armazena
+ window.alert('é um grande praze te conhecer, ' + nome)
---
+++ JavaScript - Guanabara/aula06/ex003.js
@@ -0,0 +1,10 @@
+var n1 = Number.parseFloat(window.prompt('Digite um numero: '))
+    var n2 = Number.parseFloat(window.prompt('Digite outro numero: '))
+    var s = n1 + n2
+    window.alert(`A soma entre ${n1} e ${n2} é igual a ${s}`) //concatenaçõa
+    // (number + number) para adiçõa
+    // (string + string) para concatenação
+    // template string - ( $ ) - para interpolar
+    // length -  quantos caracteres tem a string
+    // UpperCase() - para torna tud maiuscula
+    // LowerCase() - para torna tudo minuscula
